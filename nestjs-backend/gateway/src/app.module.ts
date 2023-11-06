992c1c31047c7e2bcdc46a0fdf0ce044fefb9770
SKorchinskiy <ddphyk@gmail.com>
2023-11-07 00:42:18 +0200

    + register microservices for client based TCP communication with gateway

--- nestjs-backend/gateway/src/app.module.ts
+++ nestjs-backend/gateway/src/app.module.ts
@@ -1,9 +1,49 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, TcpOptions, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH',
+        options: {
+          transport: Transport.TCP,
+          options: {
+            port: 3001,
+          },
+        } as TcpOptions,
+      },
+      {
+        name: 'PROJECT',
+        options: {
+          transport: Transport.TCP,
+          options: {
+            port: 3002,
+          },
+        } as TcpOptions,
+      },
+      {
+        name: 'USER',
+        options: {
+          transport: Transport.TCP,
+          options: {
+            port: 3003,
+          },
+        } as TcpOptions,
+      },
+      {
+        name: 'WORKSPACE',
+        options: {
+          transport: Transport.TCP,
+          options: {
+            port: 3004,
+          },
+        } as TcpOptions,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
