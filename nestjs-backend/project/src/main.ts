e5495183a79213a54ff653e9010690bc70c2a859
SKorchinskiy <ddphyk@gmail.com>
2023-11-07 00:40:44 +0200

    + setup project microservice for TCP connection

--- nestjs-backend/project/src/main.ts
+++ nestjs-backend/project/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  MicroserviceOptions,
+  Transport,
+  TcpOptions,
+} from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: 3002,
+    },
+  } as TcpOptions);
+
+  await app.startAllMicroservices();
+
+  await app.listen(3002);
 }
 bootstrap();
