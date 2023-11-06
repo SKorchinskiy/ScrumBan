d1fff71c44d6c13cdf1e673e2b636b0136a9fd3d
SKorchinskiy <ddphyk@gmail.com>
2023-11-07 00:41:03 +0200

    + setup user microservice for TCP connection

--- nestjs-backend/user/src/main.ts
+++ nestjs-backend/user/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  MicroserviceOptions,
+  Transport,
+  TcpOptions,
+} from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: 3003,
+    },
+  } as TcpOptions);
+
+  await app.startAllMicroservices();
+
+  await app.listen(3003);
 }
 bootstrap();
