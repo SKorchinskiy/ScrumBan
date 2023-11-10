ecd664fe2d2165e41aa5b319c74a70cdb73c1cd0
SKorchinskiy <ddphyk@gmail.com>
2023-11-10 23:51:44 +0200

    + fix typescript dist error

---
+++ nestjs-backend/user/src/utils/user-message-pattern.util.ts
@@ -0,0 +1,7 @@
+export enum UserMessagePattern {
+  create_user = 'create_user',
+  update_user = 'update_user',
+  find_user = 'find_user',
+  find_users = 'find_users',
+  remove_user = 'remove_user',
+}
