741dd170514d1060ce038e85ce63919f3e919214
SKorchinskiy <ddphyk@gmail.com>
2023-11-12 22:21:15 +0200

    + add project labels submodule

---
+++ nestjs-backend/project/src/label/label.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class LabelService {
+  async createProjectLabel(): Promise<any> {
+    return;
+  }
+
+  async updateProjectLabel(): Promise<any> {
+    return;
+  }
+
+  async removeProjectLabel(): Promise<any> {
+    return;
+  }
+
+  async findProjectLabels(): Promise<any> {
+    return;
+  }
+
+  async findProjectLabelByCriteria(): Promise<any> {
+    return;
+  }
+}
