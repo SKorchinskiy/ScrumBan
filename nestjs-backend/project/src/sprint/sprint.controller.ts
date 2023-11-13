d00c8e721de3c833658227df3cb081ba0adb076d
SKorchinskiy <ddphyk@gmail.com>
2023-11-14 00:00:09 +0200

    + implement sprint submodule of project microservice

--- nestjs-backend/project/src/sprint/sprint.controller.ts
+++ nestjs-backend/project/src/sprint/sprint.controller.ts
@@ -1,33 +1,52 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { SprintService } from './sprint.service';
+import { CreateSprintDto } from 'src/dto/create-sprint.dto';
+import { UpdateSprintDto } from 'src/dto/update-sprint.dto';
+import { SprintEntity } from 'src/entities/sprint.entity';
 
 @Controller('sprint')
 export class SprintController {
   constructor(private sprintService: SprintService) {}
 
   @MessagePattern({ cmd: 'create_project_sprint' })
-  async createProjectSprint(): Promise<any> {
-    return this.sprintService.createProjectSprint();
+  async createProjectSprint(
+    @Payload() payload: { projectId: number; createSprintDto: CreateSprintDto },
+  ): Promise<SprintEntity> {
+    return this.sprintService.createProjectSprint(
+      payload.projectId,
+      payload.createSprintDto,
+    );
   }
 
   @MessagePattern({ cmd: 'update_project_sprint' })
-  async updateProjectSprint(): Promise<any> {
-    return this.sprintService.updateProjectSprint();
+  async updateProjectSprint(
+    @Payload() payload: { sprintId: number; updateSprintDto: UpdateSprintDto },
+  ): Promise<SprintEntity> {
+    return this.sprintService.updateProjectSprint(
+      payload.sprintId,
+      payload.updateSprintDto,
+    );
   }
 
   @MessagePattern({ cnd: 'remove_project_sprint' })
-  async removeProjectSprint(): Promise<any> {
-    return this.sprintService.removeProjectSprint();
+  async removeProjectSprint(
+    @Payload() payload: { sprintId: number },
+  ): Promise<SprintEntity> {
+    return this.sprintService.removeProjectSprint(payload.sprintId);
   }
 
   @MessagePattern({ cmd: 'find_project_sprints' })
-  async findProjectSprints(): Promise<any> {
-    return this.sprintService.findProjectSprints();
+  async findProjectSprints(
+    @Payload() payload: { projectId: number },
+  ): Promise<SprintEntity[]> {
+    return this.sprintService.findProjectSprints(payload.projectId);
   }
 
   @MessagePattern({ cmd: 'find_project_sprint_by' })
-  async findProjectSprintByCriteria(): Promise<any> {
-    return this.sprintService.findProjectSprintByCriteria();
+  async findProjectSprintByCriteria(
+    @Payload() payload: { sprintId: number },
+  ): Promise<SprintEntity> {
+    return this.sprintService.findProjectSprintByCriteria(payload.sprintId);
   }
 }
--- nestjs-backend/project/src/sprint/sprint.service.ts
+++ nestjs-backend/project/src/sprint/sprint.service.ts
@@ -1,24 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateSprintDto } from 'src/dto/create-sprint.dto';
+import { UpdateSprintDto } from 'src/dto/update-sprint.dto';
+import { ProjectEntity } from 'src/entities/project.entity';
+import { SprintEntity } from 'src/entities/sprint.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SprintService {
-  async createProjectSprint(): Promise<any> {
-    return;
+  constructor(
+    @Inject('SPRINT_REPOSITORY')
+    private sprintRepository: Repository<SprintEntity>,
+    @Inject('PROJECT_REPOSITORY')
+    private projectRepository: Repository<ProjectEntity>,
+  ) {}
+
+  async createProjectSprint(
+    projectId: number,
+    createSprintDto: CreateSprintDto,
+  ): Promise<SprintEntity> {
+    const project = await this.projectRepository.findOne({
+      where: {
+        project_id: projectId,
+      },
+    });
+
+    const sprint = this.sprintRepository.create({
+      ...createSprintDto,
+      project,
+    });
+
+    return await this.sprintRepository.save(sprint);
   }
 
-  async updateProjectSprint(): Promise<any> {
-    return;
+  async updateProjectSprint(
+    sprintId: number,
+    updateSprintDto: UpdateSprintDto,
+  ): Promise<SprintEntity> {
+    await this.sprintRepository.update(
+      { sprint_id: sprintId },
+      updateSprintDto,
+    );
+
+    return await this.sprintRepository.findOne({
+      where: {
+        sprint_id: sprintId,
+      },
+    });
   }
 
-  async removeProjectSprint(): Promise<any> {
-    return;
+  async removeProjectSprint(sprintId: number): Promise<SprintEntity> {
+    const sprint = await this.sprintRepository.findOne({
+      where: {
+        sprint_id: sprintId,
+      },
+    });
+
+    return await this.sprintRepository.remove(sprint);
   }
 
-  async findProjectSprints(): Promise<any> {
-    return;
+  async findProjectSprints(projectId: number): Promise<SprintEntity[]> {
+    const project = await this.projectRepository.findOne({
+      where: {
+        project_id: projectId,
+      },
+    });
+
+    return await this.sprintRepository.find({
+      where: {
+        project,
+      },
+    });
   }
 
-  async findProjectSprintByCriteria(): Promise<any> {
-    return;
+  async findProjectSprintByCriteria(sprintId: number): Promise<SprintEntity> {
+    return await this.sprintRepository.findOne({
+      where: {
+        sprint_id: sprintId,
+      },
+    });
   }
 }
