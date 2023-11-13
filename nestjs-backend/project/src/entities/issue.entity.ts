32448f21bd6303540b09b02dd337063fb985ebeb
SKorchinskiy <ddphyk@gmail.com>
2023-11-14 00:03:27 +0200

    + add issue entity

--- nestjs-backend/project/src/entities/issue.entity.ts
+++ nestjs-backend/project/src/entities/issue.entity.ts
@@ -12,6 +12,7 @@ import { SprintEntity } from './sprint.entity';
 import { StateEntity } from './state.entity';
 import { LabelEntity } from './label.entity';
 import { MemberEntity } from './member.entity';
+import { ProjectEntity } from './project.entity';
 
 @Entity()
 export class IssueEntity {
@@ -46,6 +47,9 @@ export class IssueEntity {
 
   @ManyToOne(() => SprintEntity, (sprint) => sprint.issues)
   sprint: SprintEntity;
+
+  @ManyToOne(() => ProjectEntity)
+  project: ProjectEntity;
 
   @ManyToMany(() => MemberEntity)
   @JoinTable()
