b73d5ee12954113d940068a63693782145cb9cc7
SKorchinskiy <ddphyk@gmail.com>
2023-11-14 00:02:17 +0200

    + implement issue submodule of project microservice

--- nestjs-backend/project/src/issue/issue.module.ts
+++ nestjs-backend/project/src/issue/issue.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IssueController } from './issue.controller';
 import { IssueService } from './issue.service';
+import { databaseProviders } from 'src/providers/database.provider';
+import { projectProviders } from 'src/providers/project.provider';
 
 @Module({
   controllers: [IssueController],
-  providers: [IssueService],
+  providers: [IssueService, ...databaseProviders, ...projectProviders],
 })
 export class IssueModule {}
