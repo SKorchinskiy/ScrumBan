7ad6f3728ec0e648125ed6fe5299b7e0820187eb
SKorchinskiy <ddphyk@gmail.com>
2023-11-14 00:01:38 +0200

    + implement label submodule of project microservice

--- nestjs-backend/project/src/label/label.controller.ts
+++ nestjs-backend/project/src/label/label.controller.ts
@@ -1,27 +1,56 @@
 import { Controller } from '@nestjs/common';
 import { LabelService } from './label.service';
+import { Payload, MessagePattern } from '@nestjs/microservices';
+import { CreateLabelDto } from 'src/dto/create-label.dto';
+import { UpdateLabelDto } from 'src/dto/update-label.dto';
+import { LabelEntity } from 'src/entities/label.entity';
 
 @Controller('label')
 export class LabelController {
   constructor(private labelService: LabelService) {}
 
-  async createProjectLabel(): Promise<any> {
-    return this.labelService.createProjectLabel();
+  @MessagePattern({ cmd: 'create_project_label' })
+  async createProjectLabel(
+    @Payload() payload: { projectId: number; createLabelDto: CreateLabelDto },
+  ): Promise<LabelEntity> {
+    return this.labelService.createProjectLabel(
+      payload.projectId,
+      payload.createLabelDto,
+    );
   }
 
-  async updateProjectLabel(): Promise<any> {
-    return this.labelService.updateProjectLabel();
+  @MessagePattern({ cmd: 'update_project_label' })
+  async updateProjectLabel(
+    @Payload()
+    payload: {
+      labelId: number;
+      updateLabelDto: UpdateLabelDto;
+    },
+  ): Promise<LabelEntity> {
+    return this.labelService.updateProjectLabel(
+      payload.labelId,
+      payload.updateLabelDto,
+    );
   }
 
-  async removeProjectLabel(): Promise<any> {
-    return this.labelService.removeProjectLabel();
+  @MessagePattern({ cmd: 'remove_project_label' })
+  async removeProjectLabel(
+    @Payload() payload: { labelId: number },
+  ): Promise<LabelEntity> {
+    return this.labelService.removeProjectLabel(payload.labelId);
   }
 
-  async findProjectLabels(): Promise<any> {
-    return this.labelService.findProjectLabels();
+  @MessagePattern({ cmd: 'find_project_labels' })
+  async findProjectLabels(
+    @Payload() payload: { projectId: number },
+  ): Promise<LabelEntity[]> {
+    return this.labelService.findProjectLabels(payload.projectId);
   }
 
-  async findProjectLabelByCriteria(): Promise<any> {
-    return this.labelService.findProjectLabelByCriteria();
+  @MessagePattern({ cmd: 'find_project_label_by' })
+  async findProjectLabelByCriteria(
+    @Payload() payload: { labelId: number },
+  ): Promise<LabelEntity> {
+    return this.labelService.findProjectLabelByCriteria(payload.labelId);
   }
 }
