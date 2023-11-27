a06680fe716acb2cb736373be77a7450d663c283
SKorchinskiy <ddphyk@gmail.com>
2023-11-28 00:29:22 +0200

    + install cookie packages and set up jwt module with secret

--- nestjs-backend/gateway/src/main.ts
+++ nestjs-backend/gateway/src/main.ts
@@ -3,11 +3,14 @@ dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({
     origin: process.env.ALLOWED_REQUEST_ORIGIN,
+    credentials: true,
   });
   await app.listen(3000);
 }
