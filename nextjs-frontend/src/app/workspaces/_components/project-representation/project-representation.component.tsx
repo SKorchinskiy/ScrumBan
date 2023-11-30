939b16132039b37bdb6b73cf53c9e5e76b54ecf9
SKorchinskiy <ddphyk@gmail.com>
2023-11-30 02:26:37 +0200

    + add component to represent a project

---
+++ nextjs-frontend/src/app/workspaces/_components/project-representation/project-representation.component.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import styles from "./project-representation.module.css";
+import { WorkspaceProject } from "../../[workspaceId]/page";
+import { Fragment, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function ProjectRepresentation({
+  project,
+  workspaceId,
+}: {
+  project: WorkspaceProject;
+  workspaceId: number;
+}) {
+  const router = useRouter();
+  const [areProjectDetailsOpen, setAreProjectDetailsOpen] = useState(false);
+
+  const toggleAreProjectDetailsOpen = () =>
+    setAreProjectDetailsOpen(!areProjectDetailsOpen);
+
+  return (
+    <Fragment>
+      <div
+        key={project.project_id}
+        className={styles["project-representation"]}
+        onClick={() => toggleAreProjectDetailsOpen()}
+      >
+        <p style={{ userSelect: "none" }}>{project.project_name}</p>
+      </div>
+      {areProjectDetailsOpen ? (
+        <div>
+          <p
+            className={styles["project-representation"]}
+            style={{ userSelect: "none", padding: "5px" }}
+            onClick={() =>
+              router.push(
+                `/workspaces/${workspaceId}/projects/${project.project_id}/issues`
+              )
+            }
+          >
+            Issues
+          </p>
+          <p
+            className={styles["project-representation"]}
+            style={{ userSelect: "none", padding: "5px" }}
+            onClick={() =>
+              router.push(
+                `/workspaces/${workspaceId}/projects/${project.project_id}/sprints`
+              )
+            }
+          >
+            Sprints
+          </p>
+        </div>
+      ) : null}
+    </Fragment>
+  );
+}
