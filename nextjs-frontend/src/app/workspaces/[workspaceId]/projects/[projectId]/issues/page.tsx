93d1181e06077cd6b7754b4e72846a6f65ae95e8
SKorchinskiy <ddphyk@gmail.com>
2023-12-07 00:01:53 +0200

    + update issues page with functionality to update issue state on-fly with server

--- nextjs-frontend/src/app/workspaces/[workspaceId]/projects/[projectId]/issues/page.tsx
+++ nextjs-frontend/src/app/workspaces/[workspaceId]/projects/[projectId]/issues/page.tsx
@@ -5,23 +5,37 @@ import styles from "./page.module.css";
 import { usePathname } from "next/navigation";
 import { ChangeEvent, Fragment, useEffect, useMemo, useState } from "react";
 import IssueCreationalModal from "@/app/workspaces/_components/creational-modal/issue-creational-modal.component";
+import IssuesBoard from "@/app/_components/issues-board/issues-board.component";
+import StateCreationalModal from "@/app/workspaces/_components/creational-modal/state-creational-modal.component";
+
+type StateProps = {
+  state_id: number;
+  state_name: string;
+  state_color: string;
+  workspace_id: number;
+};
+
+type ProjectProps = {
+  project_id: number;
+  project_name: string;
+  project_description: string;
+  workspace_id: number;
+  project_access: string;
+};
 
 type IssueProps = {
   issue_id: number;
   issue_title: string;
   issue_description: string;
-  issue_priority: string;
-  issue_state: {
-    state_id: number;
-    state_name: string;
-    state_color: string;
-  };
-  issue_labels: Iterable<any>;
+  issue_priority: "None" | "Low" | "Medium" | "High" | "Urgent";
+  project: ProjectProps;
+  issue_state: StateProps;
 };
 
 export default function Issues() {
   const pathname = usePathname();
   const [isIssueModalOpen, setIsIssueModalOpen] = useState(false);
+  const [isStateModalOpen, setIsStateModalOpen] = useState(false);
   const workspaceId = useMemo(
     () =>
       parseInt(
@@ -41,6 +55,8 @@ export default function Issues() {
     [pathname]
   );
   const [projectIssues, setProjectIssues] = useState<IssueProps[]>([]);
+  const [filteredIssues, setFilteredIssues] =
+    useState<IssueProps[]>(projectIssues);
 
   useEffect(() => {
     const fetchProjectIssues = async () => {
@@ -55,11 +71,46 @@ export default function Issues() {
       if (response.ok) {
         const issues = await response.json();
         setProjectIssues(issues);
+        setFilteredIssues(issues);
       }
     };
 
     fetchProjectIssues();
   }, []);
+
+  const handleIssueChange = async (
+    issueId: number,
+    newStateColumnId: number
+  ) => {
+    const targetIssue = projectIssues.find(
+      (issue) => issue.issue_id === issueId
+    );
+    if (!targetIssue || !newStateColumnId) return;
+    const updateIssueDto = {
+      issue_title: targetIssue.issue_title,
+      issue_description: targetIssue.issue_description,
+      issue_priority: targetIssue.issue_priority,
+      issue_state_id: newStateColumnId,
+    };
+    const response = await fetch(
+      `http://localhost:3000/workspaces/${workspaceId}/projects/${projectId}/issues/${issueId}`,
+      {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...updateIssueDto }),
+      }
+    );
+    const updatedIssue = (await response.json()) as IssueProps;
+    const updatedIssues: IssueProps[] = projectIssues.filter(
+      (issue) => issue.issue_id !== updatedIssue.issue_id
+    );
+    updatedIssues.push(updatedIssue);
+    setProjectIssues(updatedIssues);
+    setFilteredIssues(updatedIssues);
+  };
 
   return (
     <div
@@ -76,7 +127,7 @@ export default function Issues() {
           display: "flex",
           flexDirection: "column",
           backgroundColor: "#392f53",
-          width: "95%",
+          width: "950px",
           height: "95%",
           borderRadius: "10px",
         }}
@@ -84,35 +135,20 @@ export default function Issues() {
         <PanelHeader
           inputPlaceholder="Type to filter project issues..."
           creationalButtonText="Create Project Issue"
-          onInputChangeHandler={(event: ChangeEvent<HTMLInputElement>) => {}}
+          onInputChangeHandler={(event: ChangeEvent<HTMLInputElement>) => {
+            const value = event.target.value;
+            setFilteredIssues(
+              projectIssues.filter((issue) =>
+                issue.issue_title.toLowerCase().includes(value)
+              )
+            );
+          }}
           creationalButtonHandler={() => setIsIssueModalOpen(true)}
         />
-        <div
-          style={{
-            backgroundColor: "#392F53",
-            width: "95%",
-            height: "95%",
-            borderRadius: "10px",
-            overflow: "scroll",
-          }}
-        >
-          {projectIssues.map((issue) => (
-            <div key={issue.issue_id} className={styles["issues-container"]}>
-              <div className={styles["issue-details"]}>
-                <p>{issue.issue_title}</p>
-              </div>
-              <div className={styles["issue-details"]}>
-                <p>{issue.issue_description}</p>
-              </div>
-              <div className={styles["issue-details"]}>
-                <p>{issue.issue_priority}</p>
-              </div>
-              <div className={styles["issue-details"]}>
-                <p>{issue?.issue_state?.state_name}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <IssuesBoard
+          issues={filteredIssues}
+          handleIssueChange={handleIssueChange}
+        />
       </div>
       {isIssueModalOpen ? (
         <Fragment>
@@ -137,7 +173,37 @@ export default function Issues() {
             <IssueCreationalModal
               workspaceId={workspaceId}
               project_id={projectId}
-              onCancelHandler={() => setIsIssueModalOpen(false)}
+              onCancelHandler={(shouldStateModalOpen: boolean) => {
+                setIsIssueModalOpen(false);
+                setIsStateModalOpen(shouldStateModalOpen);
+              }}
+            />
+          </div>
+        </Fragment>
+      ) : null}
+      {isStateModalOpen ? (
+        <Fragment>
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <StateCreationalModal
+              workspaceId={workspaceId}
+              onCancelHandler={() => setIsStateModalOpen(false)}
             />
           </div>
         </Fragment>
