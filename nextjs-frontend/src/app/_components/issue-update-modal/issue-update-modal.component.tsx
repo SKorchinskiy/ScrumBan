3c9e2685df48d5eea36faa2c380c4a1f3cae65e3
SKorchinskiy <ddphyk@gmail.com>
2023-12-08 22:28:53 +0200

    + add modal to update an issue

---
+++ nextjs-frontend/src/app/_components/issue-update-modal/issue-update-modal.component.tsx
@@ -0,0 +1,196 @@
+"use client";
+
+import { ChangeEvent, MouseEvent, useEffect, useMemo, useState } from "react";
+import styles from "./issue-update-modal.module.css";
+import { WorkspaceState } from "@/app/workspaces/_components/sidebar/sidebar.component";
+import { IssueProps } from "@/app/workspaces/[workspaceId]/projects/[projectId]/sprints/[sprintId]/page";
+
+export type IssueParams = {
+  issue_title: string;
+  issue_description: string;
+  issue_priority: "None" | "Low" | "Medium" | "High" | "Urgent";
+  issue_state_id: number;
+};
+
+export default function IssueUpdateModal({
+  issue,
+  workspaceId,
+  onCancelHandler,
+}: {
+  issue: Partial<IssueProps>;
+  workspaceId: number;
+  onCancelHandler: Function;
+} & { project_id?: number }) {
+  const [states, setStates] = useState<WorkspaceState[]>([]);
+  const issueId = useMemo(() => issue.issue_id, [issue]);
+  const [issueInfo, setIssueInfo] = useState<IssueParams>({
+    issue_title: issue.issue_title!,
+    issue_description: issue.issue_description!,
+    issue_priority: issue.issue_priority!,
+    issue_state_id: issue.issue_state!.state_id,
+  });
+
+  useEffect(() => {
+    const fetchWorkspaceStates = async () => {
+      const response = await fetch(
+        `http://localhost:3000/workspaces/${workspaceId}/states`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      if (response.ok) {
+        const states = await response.json();
+        setStates(states);
+      }
+    };
+
+    fetchWorkspaceStates();
+  }, []);
+
+  const setIssueInputData = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setIssueInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const updateIssue = async (event: MouseEvent<HTMLButtonElement>) => {
+    await fetch(
+      `http://localhost:3000/workspaces/${workspaceId}/issues/${issueId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          ...issueInfo,
+        }),
+      }
+    );
+    onCancelHandler();
+  };
+
+  return (
+    <div
+      className={styles["modal-container"]}
+      style={{
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          top: 20,
+          right: 20,
+          width: "10px",
+          height: "10px",
+          background: "rgba(24, 18, 41, 0.3)",
+          borderRadius: "5px",
+          padding: "10px",
+          cursor: "pointer",
+          userSelect: "none",
+        }}
+        onClick={() => onCancelHandler()}
+      >
+        <p
+          style={{
+            color: "white",
+          }}
+        >
+          &#x2715;
+        </p>
+      </div>
+      <h1 className={styles["heading"]}>Update Issue</h1>
+      <div className={styles["input-container"]}>
+        <input
+          type="text"
+          name="issue_title"
+          className={styles["modal-input"]}
+          value={issueInfo.issue_title}
+          onChange={setIssueInputData}
+          placeholder="Enter your issue name..."
+        />
+        <input
+          type="text"
+          name="issue_description"
+          className={styles["modal-input"]}
+          value={issueInfo.issue_description}
+          onChange={setIssueInputData}
+          placeholder="Enter your issue description..."
+        />
+        <select
+          className={styles["modal-input"]}
+          name="issue_priority"
+          onChange={setIssueInputData}
+        >
+          <option value="" disabled selected>
+            Select issue priority
+          </option>
+          <option value="None" selected={issueInfo.issue_priority === "None"}>
+            None
+          </option>
+          <option value="Low" selected={issueInfo.issue_priority === "Low"}>
+            Low
+          </option>
+          <option
+            value="Medium"
+            selected={issueInfo.issue_priority === "Medium"}
+          >
+            Medium
+          </option>
+          <option value="High" selected={issueInfo.issue_priority === "High"}>
+            High
+          </option>
+          <option
+            value="Urgent"
+            selected={issueInfo.issue_priority === "Urgent"}
+          >
+            Urgent
+          </option>
+        </select>
+        <select
+          name="issue_state"
+          className={styles["modal-input"]}
+          onChange={(event) => {
+            if (event.target.selectedOptions[0].value == "update")
+              onCancelHandler();
+            setIssueInfo((prev) => ({
+              ...prev,
+              issue_state_id: parseInt(event.target.selectedOptions[0].value),
+            }));
+          }}
+        >
+          <option value="" disabled selected>
+            Select issue state
+          </option>
+          {states.map((state) => (
+            <option
+              key={state.state_id}
+              value={state.state_id}
+              selected={issueInfo.issue_state_id === state.state_id}
+            >
+              {state.state_name}
+            </option>
+          ))}
+          <option value="update">+ Update issue state</option>
+        </select>
+        <button
+          className={styles["default-button"]}
+          onClick={(e) => updateIssue(e)}
+        >
+          update issue
+        </button>
+      </div>
+    </div>
+  );
+}
