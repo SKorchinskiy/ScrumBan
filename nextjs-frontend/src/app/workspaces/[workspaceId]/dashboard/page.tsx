6f6d8c17eed3635cb487fb1afbcf2e8db8882da7
SKorchinskiy <ddphyk@gmail.com>
2023-12-10 21:17:30 +0200

    + add dashboard page

---
+++ nextjs-frontend/src/app/workspaces/[workspaceId]/dashboard/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import styles from "./page.module.css";
+import ActivityGraph from "./_components/activity-graph/activity-graph.component";
+import ActivityOverview from "./_components/activity-overview/activity-overview.component";
+import { useEffect, useMemo, useState } from "react";
+import { ProjectProps } from "../projects/[projectId]/sprints/[sprintId]/page";
+import { usePathname } from "next/navigation";
+
+export default function Dashboard() {
+  const [workspaceProjects, setWorkspaceProjects] = useState<ProjectProps[]>(
+    []
+  );
+
+  const pathname = usePathname();
+  const currentHour = useMemo(
+    () => parseInt(new Date(Date.now()).toTimeString().split(":")[0]),
+    []
+  );
+  const dayPeriod = useMemo(
+    () =>
+      currentHour < 12 && currentHour >= 5
+        ? "morning"
+        : currentHour >= 12 && currentHour < 18
+        ? "afternoon"
+        : "evening",
+    [currentHour]
+  );
+  const workspaceId = useMemo(() => {
+    const pathChunks = pathname.split("/");
+    const workspaceIndex = pathChunks.findIndex((chunk) =>
+      chunk.includes("workspaces")
+    );
+    return parseInt(pathChunks[workspaceIndex + 1]);
+  }, [pathname]);
+
+  useEffect(() => {
+    const fetchWOrkspaceProjects = async () => {
+      const response = await fetch(
+        `http://localhost:3000/workspaces/${workspaceId}/projects`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      const projects = await response.json();
+      setWorkspaceProjects(projects);
+    };
+
+    fetchWOrkspaceProjects();
+  }, [workspaceId]);
+
+  return (
+    <div className={styles["specific-workspace-container"]}>
+      <div className={styles["specific-workspace-board"]}>
+        <h1 className={styles["user-greeting"]}>Good {dayPeriod}, dear user</h1>
+        <ActivityOverview
+          projects={workspaceProjects}
+          workspaceId={workspaceId}
+        />
+        <ActivityGraph />
+      </div>
+    </div>
+  );
+}
