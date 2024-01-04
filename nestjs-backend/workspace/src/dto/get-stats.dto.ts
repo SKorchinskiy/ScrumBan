33eb5d18b6e680730ace12e60ce00a688a7c9a7a
SKorchinskiy <ddphyk@gmail.com>
2024-01-04 23:58:19 +0200

    + add stats dto's to the workspace microservice

---
+++ nestjs-backend/workspace/src/dto/get-stats.dto.ts
@@ -0,0 +1,4 @@
+export type StatsDto = {
+  workspaceId: number;
+  limit: number;
+};
--- nestjs-backend/workspace/src/workspace.module.ts
+++ nestjs-backend/workspace/src/workspace.module.ts
@@ -3,10 +3,18 @@ import { WorkspaceController } from './workspace.controller';
 import { WorkspaceService } from './workspace.service';
 import { databaseProviders } from './providers/database.provider';
 import { workspaceProviders } from './providers/workspace.provider';
+import { StatsModule } from './stats/stats.module';
+import { StatsController } from './stats/stats.controller';
+import { StatsService } from './stats/stats.service';
 
 @Module({
-  imports: [],
-  controllers: [WorkspaceController],
-  providers: [WorkspaceService, ...databaseProviders, ...workspaceProviders],
+  imports: [StatsModule],
+  controllers: [StatsController, WorkspaceController],
+  providers: [
+    StatsService,
+    WorkspaceService,
+    ...databaseProviders,
+    ...workspaceProviders,
+  ],
 })
 export class AppModule {}
